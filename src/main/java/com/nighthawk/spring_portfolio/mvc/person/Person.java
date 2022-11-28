158bc7808329ffba42e5feb28dd594a80e347c61
alice <tang.alice.000@gmail.com>
2022-11-27 21:51:54 -0800

    person

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
@@ -1,12 +1,10 @@
 package com.nighthawk.spring_portfolio.mvc.person;
-
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,18 +13,14 @@ import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import com.vladmihalcea.hibernate.type.json.JsonType;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-
 /*
 Person is a POJO, Plain Old Java Object.
 First set of annotations add functionality to POJO
@@ -40,31 +34,30 @@ The last annotation connect to database
 @Entity
 @TypeDef(name="json", typeClass = JsonType.class)
 public class Person {
-    
     // automatic unique identifier for Person record
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     // email, password, roles are key attributes to login and authentication
     @NotEmpty
     @Size(min=5)
     @Column(unique=true)
     @Email
     private String email;
-
     @NotEmpty
     private String password;
-
     // @NonNull, etc placed in params of constructor: "@NonNull @Size(min = 2, max = 30, message = "Name (2 to 30 chars)") String name"
     @NonNull
     @Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
     private String name;
-
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
-    
-
+    @Column(unique=false)
+    private int height;
+    @Column(unique=false)
+    private int weight;
+    @NotEmpty
+    private String gender;
     /* HashMap is used to store JSON for daily "stats"
     "stats": {
         "2022-11-13": {
@@ -75,17 +68,17 @@ public class Person {
     */
     @Type(type="json")
     @Column(columnDefinition = "jsonb")
-    private Map<String,Map<String, Object>> stats = new HashMap<>(); 
-    
-
+    private Map<String,Map<String, Object>> stats = new HashMap<>();
     // Constructor used when building object from an API
-    public Person(String email, String password, String name, Date dob) {
+    public Person(String email, String password, String name, Date dob, int height, int weight, String gender) {
         this.email = email;
         this.password = password;
         this.name = name;
         this.dob = dob;
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
     }
-
     // A custom getter to return age from dob attribute
     public int getAge() {
         if (this.dob != null) {
@@ -93,5 +86,49 @@ public class Person {
             return Period.between(birthDay, LocalDate.now()).getYears(); }
         return -1;
     }
+    public double getBmi() {
+            double bmi = (703 * this.weight / Math.pow(this.height,2));
+            return bmi;
+    }
 
+    public double bmrCalculator() {
+        if (getGender().equals("male")) {
+            return 88.362 + (13.397 * getWeight() / 2.205) + (4.799 * getHeight() * 2.54) - (5.677 * getAge());
+        } else if (getGender().equals("female")) {
+            return 447.593 + (9.247 * getWeight() / 2.205) + (3.098 * getHeight() * 2.54) - (4.330 * getAge());
+        } else {
+            return -1;
+        }
+    }
+    public double amrCalculator() {
+        return bmrCalculator() * 1.55;
+    }
+    // Calories required to burn per day to maintain current weight (Uses Active &
+    // Basal Metabolic Rate)
+    public int getStep() {
+        if (getDob() != null && getWeight() != 0 && getHeight() != 0) {
+            // assumes moderate exercise each day, indicates calories needed to burn
+            return (int) (Math.round(amrCalculator() - bmrCalculator() - 500) / 0.04);
+        }
+        return -1;
+    }
+    public String getBmiToString() {
+        return("{ \"bmi\": " + this.getBmi() +"}");
+    }
+
+    public String getStepToString() {
+        return("{ \"step\": " + this.getStep() +"}");
+    }
+
+    public String toString(){
+        return ("{\"email\": " + this.email + ", " + "\"password\": " + this.password + ", " + "\"name\": " + this.name + ", " + "\"dob\": " + this.dob + " \"height\": " + this.height + ", \"weight\": " + this.weight + "}" );
+    } 
+    public static void main(String[] args){
+        LocalDate dob = LocalDate.of(2006,07,19);
+        Date date = Date.from(dob.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Person noArgsPerson = new Person();
+        Person allArgsPerson = new Person("samaya2024@gmail.com","fruitloops", "Samaya Sankuratri", date, 68, 117, "female");
+        System.out.println(noArgsPerson);
+        System.out.println(allArgsPerson);
+    }
 }
--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
@@ -43,6 +43,30 @@ public class PersonApiController {
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);       
     }
 
+    @GetMapping("/getBmi/{id}")
+    public String getBmi(@PathVariable long id) {
+        Optional<Person> optional = repository.findById(id);
+        if (optional.isPresent()) {  // Good ID
+            Person person = optional.get();  // value from findByID
+            String bmiToString = person.getBmiToString();
+            return bmiToString;
+        }
+        // Bad ID
+        return "error";       
+    }
+
+    @GetMapping("/getStep/{id}")
+    public String getStep(@PathVariable long id) {
+        Optional<Person> optional = repository.findById(id);
+        if (optional.isPresent()) {  // Good ID
+            Person person = optional.get();  // value from findByID
+            String stepToString = person.getStepToString();
+            return stepToString;
+        }
+        // Bad ID
+        return "error";       
+    }
+
     /*
     DELETE individual Person using ID
      */
@@ -65,7 +89,10 @@ public class PersonApiController {
     public ResponseEntity<Object> postPerson(@RequestParam("email") String email,
                                              @RequestParam("password") String password,
                                              @RequestParam("name") String name,
-                                             @RequestParam("dob") String dobString) {
+                                             @RequestParam("dob") String dobString,
+                                             @RequestParam("height") int height,
+                                             @RequestParam("weight") int weight,
+                                             @RequestParam("gender") String gender) {
         Date dob;
         try {
             dob = new SimpleDateFormat("MM-dd-yyyy").parse(dobString);
@@ -73,7 +100,7 @@ public class PersonApiController {
             return new ResponseEntity<>(dobString +" error; try MM-dd-yyyy", HttpStatus.BAD_REQUEST);
         }
         // A person object WITHOUT ID will create a new record with default roles as student
-        Person person = new Person(email, password, name, dob);
+        Person person = new Person(email, password, name, dob, height, weight, gender);
         repository.save(person);
         return new ResponseEntity<>(email +" is created successfully", HttpStatus.CREATED);
     }
@@ -97,33 +124,33 @@ public class PersonApiController {
     The personStats API adds stats by Date to Person table 
     */
     @PostMapping(value = "/setStats", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Person> personStats(@RequestBody final Map<String,Object> stat_map) {
+    public ResponseEntity<Person> personStats(@RequestBody final Map<String, Object> stat_map) {
         // find ID
-        long id=Long.parseLong((String)stat_map.get("id"));  
+        long id = Long.parseLong((String) stat_map.get("id"));
         Optional<Person> optional = repository.findById((id));
-        if (optional.isPresent()) {  // Good ID
-            Person person = optional.get();  // value from findByID
+        if (optional.isPresent()) { // Good ID
+            Person person = optional.get(); // value from findByID
 
             // Extract Attributes from JSON
             Map<String, Object> attributeMap = new HashMap<>();
-            for (Map.Entry<String,Object> entry : stat_map.entrySet())  {
-                // Add all attribute other thaN "date" to the "attribute_map"
+            for (Map.Entry<String, Object> entry : stat_map.entrySet()) {
+                // Add all attribute other than "date" to the "attribute_map"
                 if (!entry.getKey().equals("date") && !entry.getKey().equals("id"))
                     attributeMap.put(entry.getKey(), entry.getValue());
             }
 
             // Set Date and Attributes to SQL HashMap
-            Map<String, Map<String, Object>> date_map = new HashMap<>();
-            date_map.put( (String) stat_map.get("date"), attributeMap );
-            person.setStats(date_map);  // BUG, needs to be customized to replace if existing or append if new
-            repository.save(person);  // conclude by writing the stats updates
+            Map<String, Map<String, Object>> date_map = person.getStats();
+            date_map.put((String) stat_map.get("date"), attributeMap);
+            person.setStats(date_map); // BUG, needs to be customized to replace if existing or append if new
+            repository.save(person); // conclude by writing the stats updates
 
             // return Person with update Stats
             return new ResponseEntity<>(person, HttpStatus.OK);
         }
         // return Bad ID
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
-        
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
     }
 
 }
